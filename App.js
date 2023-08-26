c4091a2d9372a7033da1f13cf635bc3d4518aecc
Edward <edward.nayve@boom.ai>
2023-08-26 19:10:35 +0800

    Initial app setup

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { Camera } from 'expo-camera';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
+  const [cameraPermission, setCameraPermission] = Camera.useCameraPermissions()
+  const [type, setTypes] = useState(Camera.Constants.Type.back);
+  const [cameraRef, setCameraRef] = useState(null);
+
+  if (cameraPermission === null) {
+    setCameraPermission()
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Camera app template</Text>
+      <View style={styles.cameraContainer}>
+        <Camera style={styles.camera} type={type} ref={ref => setCameraRef(ref)} />
+      </View>
+      <TouchableOpacity style={{ width: 100 }} onPress={() => alert()}>
+        <Text>Capture Image</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +35,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  cameraContainer: {
+    width: 240,
+    height: 320,
+    borderRadius: 20,
+    border: '1px solid red',
+    padding: 20,
+  },
+  camera: {
+  }
 });
