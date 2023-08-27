76b705d593bf5af24a1c3f4ad14cec8e365ab4a0
Edward <edward.nayve@boom.ai>
2023-08-27 14:56:07 +0800

    update fonts and add icons

--- App.js
+++ App.js
@@ -1,24 +1,31 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Camera } from 'expo-camera';
-import React from 'react';
-import { StyleSheet, Text, View, Pressable } from 'react-native';
-
+import { StyleSheet, Text, View, Pressable, Image} from 'react-native';
+import { useFonts } from 'expo-font';
+import { ConfigIcon, ManIcon } from './assets/images';
 
 export default function App() {
   const [cameraPermission, setCameraPermission] = Camera.useCameraPermissions()
   const [type, setTypes] = useState(Camera.Constants.Type.back);
   const [cameraActive, setCameraActive] = useState(false)
   const [cameraRef, setCameraRef] = useState(null);
+  const [fontsLoaded] = useFonts({
+    'sao-font': require('./assets/fonts/sao-font.ttf')
+  })
 
   const handleCamera = () => {
     setCameraPermission()
     setCameraActive(true)
   }
 
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Camera app template</Text>
+      <Text style={styles.header}>Mobile App</Text>
       {
         cameraActive ?
           <View style={styles.cameraContainer}>
@@ -32,9 +39,11 @@ export default function App() {
           </View>
           :
           <View style={styles.sectionContainer}>
-            <Text>Upload a file</Text>
+            <Pressable onPress={() => alert()}>
+              <Image source={ManIcon} style={styles.image}/>
+            </Pressable>
             <Pressable onPress={handleCamera}>
-              <Text>Use camera</Text>
+              <Image source={ConfigIcon} style={styles.image}/>
             </Pressable>
           </View>}
       <StatusBar style="auto" />
@@ -47,18 +56,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
   header: {
-    top: 10,
-    color: 'red',
-    fontSize: 16
+    fontSize: 24,
+    fontFamily: 'sao-font'
   },
   sectionContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: 240,
-    height: 320,
     alignItems: 'center'
   },
   cameraContainer: {
@@ -70,5 +77,9 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   camera: {
+  },
+  image: {
+    height: 100,
+    width: 100,
   }
 });
---
+++ assets/images/index.js
@@ -0,0 +1,5 @@
+import ConfigIcon from "./Config.png"
+import ManIcon from "./Man.png"
+
+
+export { ConfigIcon, ManIcon }
